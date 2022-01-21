91ab2d84e7aed99b566c67e3a40922a6c9b44805
prachanapraveen <praveenjp484@gmail.com>
2022-01-21 11:55:42 +0530

    sucess

---
+++ src/org/bike/Ktm.java
@@ -0,0 +1,21 @@
+package org.bike;
+
+public class Ktm{
+
+	
+	public void cost() {
+	System.out.println("2.50000");
+		
+	}
+
+	
+	public void speed() {
+	System.out.println("145");
+	}
+public static void main(String[] args) {
+	Ktm k = new Ktm();
+	k.cost();
+	k.speed();
+}
+
+}
